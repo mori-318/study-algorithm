7720ffaedc9cf75a0247ec097b2fd0cc77b7d8c2
mori-318 <minotuyu31851423520@gmail.com>
2025-05-08 14:28:27 +0900

    3_1実装　＆　.gitignoreにC++のコンパイル成果配置用フォルダを記載

--- chapter_03/c++/3_1.cpp
+++ chapter_03/c++/3_1.cpp
@@ -1,9 +1,16 @@
-#include <iostream>  // 追加
-#include <string>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    int target_num = 23;
+    int list_a[] = {1, 23, 542, 656, 88, 4, 3, 6, 4, 33, 32, 23};
+
+    int max_idx = 0;
+    for (int i = 0; i < sizeof(list_a) / sizeof(list_a[0]); i++) {
+        if (list_a[i] == target_num) {
+            max_idx = i;
+        }
+    }
+    cout <<"最大のインデックスは、" << max_idx << endl;
 }
